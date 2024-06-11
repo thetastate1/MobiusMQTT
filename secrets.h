55e65adb7ba5233003a494b3e1c3fd899a87ec86
Mark Williams <theta.state@hotmail.com>
2024-06-11 18:45:39 +1000

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,16 @@
+// Keep the double quotes ""
+// Your MQTT server
+#define mqttServer "insert your mqtt broker hostname or ip address here"
+
+// Your WiFi SSID
+#define mySSID "insert your wifi SSID here"
+
+// Your WiFi key
+#define myPassword "insert your wifi password here"
+
+// mqtt username Can be omitted if not needed 
+#define mqttUser ""
+
+// mqtt pass Can be omitted if not needed
+#define mqttPass ""
+
